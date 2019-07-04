82638bbab35d1280e76f199b1e21d10aaab10ff8
Cameron Eagans <me@cweagans.net>
2019-07-03 23:17:55 -0600

    Use /app so that the user sees the app or a login screen, rather than the marketing site

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cat ./counter.js > todoist.js; cat ./todoist-shortcuts-28/src/todoist-shortcuts.js >> todoist.js
-nativefier 'https://todoist.com/' --name 'Todoist' --icon ./todoist-icon.png --inject ./todoist.js --counter --bounce --single-instance --overwrite
+nativefier 'https://todoist.com/app' --name 'Todoist' --icon ./todoist-icon.png --inject ./todoist.js --counter --bounce --single-instance --overwrite
