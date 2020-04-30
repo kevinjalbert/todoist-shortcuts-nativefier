4a36902cc5d92c8720edf1f0efcd27ac4c1682cc
Kevin Jalbert <kevin.j.jalbert@gmail.com>
2020-04-29 23:15:32 -0400

    feat: use node_modules nativefier bin instead of systems
    Before the assumption was made that the executable was on your path,
    in which it might not be. The `npm install` will put it in the
    node_modules though.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git submodule update --init --recursive
 cat ./counter.js > todoist.js; cat ./todoist-shortcuts/src/todoist-shortcuts.js >> todoist.js
-nativefier 'https://todoist.com/app' --name 'Todoist' --icon ./todoist-icon.png --inject ./todoist.js --counter --bounce --single-instance --overwrite
+./node_modules/.bin/nativefier 'https://todoist.com/app' --name 'Todoist' --icon ./todoist-icon.png --inject ./todoist.js --counter --bounce --single-instance
